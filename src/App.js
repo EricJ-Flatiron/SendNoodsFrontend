a731534f5409e25356f4728804e4156d7daf600c
Eric Johnson <johnson.n.eric@gmail.com>
2020-07-16 12:22:25 -0500

    first

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import './App.css';
+import Login from './components/auth/login.js';
+import SignUp from './components/auth/SignUp.js';
+
+class App extends React.Component {
+
+  state = {
+    isLoading: true,
+    token: localStorage.token,
+    user: JSON.parse(localStorage.getItem("user"))
+  }
+
+  componentDidMount(){
+    this.setState({
+      isLoading: false
+    })
+  }
+
+  handleStateChanges = (key, value) => {
+    this.setState({
+      [key]: value
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isLoading
+        ? <h4> Loading... </h4>
+        :<Router>
+
+          <Route exact path="/">
+
+          </Route>
+
+          <Route exact path="/signup" component={() => 
+            <SignUp>
+
+            </SignUp>}>
+          </Route>
+    
+    
+    
+    
+        </Router>
+        }  
+      </div>
+    );
+  }
+}
+
+export default App;
